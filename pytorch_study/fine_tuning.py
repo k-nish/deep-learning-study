472681d9613d2ab857a10a358559b6ae70fd0f12
Kohei Nishimura <koh.oct.un@gmail.com>
2018-03-06 00:32:32 +0900

    [add] fixed model weight

--- pytorch_study/fine_tuning.py
+++ pytorch_study/fine_tuning.py
@@ -132,6 +132,28 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
 torch.save(model_ft.state_dict(), 'model_ft.pkl')
 
+'''
+fix model weights
+'''
+model_conv = models.resnet18(pretrained=True)
+
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+# update last layer
+num_features = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_features, 2)
+
+if use_gpu:
+    model_conv = model_conv.cuda()
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer_conv = optim.SGD(model_conv.parameters(), lr=0.001, momentum=0.9)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=25)
+torch.save(model_conv.state_dict(), 'model_conv.pkl')
+
 
 '''
 visualize train image
