69e1f9348679b5312f7a0be6c8775014da905ed1
Kohei Nishimura <koh.oct.un@gmail.com>
2018-03-06 00:10:11 +0900

    [add] fine_tuning.py

---
+++ pytorch_study/fine_tuning.py
@@ -0,0 +1,196 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import torchvision
+from torchvision import datasets, models, transforms
+
+import os
+import time
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizeCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.RandomResizeCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+
+
+data_dir = os.path.join('data', 'hymenoptera_data')
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(
+                    image_datasets[x],
+                    batch_size=4,
+                    shuffle=True,
+                    num_workers=4)
+               for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+use_gpu = torch.cuda.is_available()
+
+if use_gpu:
+    print('cuda is available!')
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # train and validate in each epoch
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)
+            else:
+                model.train(False)
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for data in dataloaders[phase]:
+                inputs, labels = data
+
+                if use_gpu:
+                    inputs = Variable(inputs.cuda())
+                    labels = Variable(labels.cuda())
+                else:
+                    inputs = Variable(inputs)
+                    labels = Variable(labels)
+
+                optimizer.zero_grad()
+
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # statistics
+                running_loss += loss.data[0] * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val acc: {:.4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+model_ft = models.resnet18(pretrained=True)
+print(model_ft)
+
+num_features = model_ft.fc.in_features
+print(num_features)
+
+model_ft.fc = nn.Linear(num_features, 2)
+print(model_ft)
+
+if use_gpu:
+    model_ft = model_ft.cuda()
+
+criterion = nn.CrossEntropyLoss()
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+# 0.1 times by 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+torch.save(model_ft.state_dict(), 'model_ft.pkl')
+
+
+'''
+visualize train image
+def imshow(image, title=None):
+    images = images.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    images = std * images + mean
+    images = np.clip(images, 0, 1)
+    plt.imshow(images)
+    if title is not None:
+        plt.title(title)
+
+images, classes = next(iter(dataloaders['train']))
+print(images.size(), classes.size())  # torch.Size([4, 3, 224, 224]) torch.Size([4])
+images = torchvision.utils.make_grid(images)
+imshow(images, title=[class_names[x] for x in classes])
+'''
+
+# data augmentation example
+'''
+data_dir = os.path.join('data', 'hymenoptera_data')
+image_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'))
+print(len(image_dataset))
+'''
+'''
+image, label = image_dataset[0]
+'''
+'''
+RandomResizeCrop
+plt.figure()
+plt.imshow(image)
+
+t = transforms.RandomResizeCrop(224)
+trans_image = t(image)
+
+plt.figure()
+plt.imshow(trans_image)
+'''
+
+'''
+RandomHorizontalFlip
+plt.figure()
+plt.imshow(image)
+
+t = transforms.RandomHorizontalFlip()
+trans_image = t(image)
+
+plt.figure()
+plt.imshow(trans_image)
+'''
+
+'''
+centercrop
+plt.figure()
+plt.imshow(image)
+t = transforms.CenterCrop(224)
+trans_image = t(image)
+
+plt.figure()
+plt.imshow(trans_image)
+'''
